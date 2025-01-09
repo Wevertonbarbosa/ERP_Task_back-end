7bcbaa265ed446784e4694ad126c291a3b7cf8a7
Wevertonbarbosa <wevertonlbs00@gmail.com>
2025-01-09 00:19:39 -0300

    ajustando o envio de email

--- src/main/java/br/com/happydo/service/RedefinirSenhaService.java
+++ src/main/java/br/com/happydo/service/RedefinirSenhaService.java
@@ -15,24 +15,19 @@ public class RedefinirSenhaService {
     @Autowired
     private EmailUsuarioService emailUsuarioService;
 
-
-
-
-
     //Redefinicao de senha
     public void redefinicaoSenha(String emailUsuario) {
         if (usuarioRepository.existsByEmail(emailUsuario)) {
             // Gerar o link para redefinir a senha (aqui estamos usando uma URL simples)
-            String linkRedefinicao = "http://localhost:8080/redefinir-senha?email=" + emailUsuario;
+            String linkRedefinicao = "http://localhost:4200/redefinir";
             String assunto = "Solicitação de Redefinição de Senha";
-            String mensagem = "Você solicitou a redefinição de sua senha. Clique no link abaixo para redefinir a sua senha:\n" + linkRedefinicao;
+            String mensagem = "Para redefinir sua senha,\nClique no link e seja redirecionado: " + linkRedefinicao;
 
             emailUsuarioService.enviarEmail(emailUsuario, assunto, mensagem);
         } else {
             throw new UsuarioNaoEncontradoException("Usuário não encontrado.");
         }
     }
-
 
     //Validacao e atualizacao de senha
     public void novaSenha(String email, String novaSenha) {
